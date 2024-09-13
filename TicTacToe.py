88b94e8eeba447761429659964c39ab2640a4abc
Santiago Nieto <A01612348@tec.mx>
2024-09-13 12:42:23 -0600

    Verificar con flake8

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,6 @@
-from turtle import *
-
+from turtle import (color, up, goto, down,
+                    circle, setup, hideturtle, tracer,
+                    update, onscreenclick, done)
 from freegames import line
 
 
@@ -13,31 +14,32 @@ def grid():
 
 def drawx(x, y):
     """Draw X player."""
-    color("red")  # Cambiar color de "X"
-    line(x + 20, y + 20, x + 113, y + 113)  # Ajustar tamaño y posición
+    color("red")
+    line(x + 20, y + 20, x + 113, y + 113)
     line(x + 20, y + 113, x + 113, y + 20)
 
 
 def drawo(x, y):
     """Draw O player."""
     up()
-    goto(x + 67, y + 30)  # Ajustar posición para centrar
+    goto(x + 67, y + 30)
     down()
-    color("blue")  # Cambiar color de "O"
-    circle(50)  # Ajustar tamaño del círculo
+    color("blue")
+    circle(50)
 
 
 def floor(value):
     """Round value down to grid with square size 133."""
     return ((value + 200) // 133) * 133 - 200
 
+
 board = {}
+
 
 def tap(x, y):
     """Draw X or O in tapped square."""
     x = floor(x)
     y = floor(y)
-    
     if (x, y) in board:
         print("Casilla ya ocupada")
         return
@@ -45,9 +47,7 @@ def tap(x, y):
     player = state['player']
     draw = players[player]
     draw(x, y)
-    
     board[(x, y)] = player
-    
     update()
     state['player'] = not player
 
