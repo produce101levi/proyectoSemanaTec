9be249a98dfd4e33a0b03be44f6026d8c856383a
Santiago Nieto <A01612348@tec.mx>
2024-09-13 12:28:58 -0600

    Validar Casillas en el juego

--- TicTacToe.py
+++ TicTacToe.py
@@ -13,39 +13,47 @@ def grid():
 
 def drawx(x, y):
     """Draw X player."""
-    color("red")
-    line(x + 20, y + 20, x + 113, y + 113)
+    color("red")  # Cambiar color de "X"
+    line(x + 20, y + 20, x + 113, y + 113)  # Ajustar tamaño y posición
     line(x + 20, y + 113, x + 113, y + 20)
 
 
 def drawo(x, y):
     """Draw O player."""
     up()
-    goto(x + 67, y + 30)
+    goto(x + 67, y + 30)  # Ajustar posición para centrar
     down()
-    color("blue")
-    circle(50)
+    color("blue")  # Cambiar color de "O"
+    circle(50)  # Ajustar tamaño del círculo
 
 
 def floor(value):
     """Round value down to grid with square size 133."""
     return ((value + 200) // 133) * 133 - 200
 
-
-state = {'player': 0}
-players = [drawx, drawo]
-
+board = {}
 
 def tap(x, y):
     """Draw X or O in tapped square."""
     x = floor(x)
     y = floor(y)
+    
+    if (x, y) in board:
+        print("Casilla ya ocupada")
+        return
+
     player = state['player']
     draw = players[player]
     draw(x, y)
+    
+    board[(x, y)] = player
+    
     update()
     state['player'] = not player
 
+
+state = {'player': 0}
+players = [drawx, drawo]
 
 setup(420, 420, 370, 0)
 hideturtle()
